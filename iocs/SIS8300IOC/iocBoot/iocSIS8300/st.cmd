cb04aa6d2caf31f8687ce47bd0adc16a7fbbe074
Hinko Kocevar <hinko.kocevar@esss.se>
2017-01-26 11:21:34 +0100

    Remove extra st.cmd.

--- iocs/SIS8300IOC/iocBoot/iocSIS8300/st.cmd
+++ iocs/SIS8300IOC/iocBoot/iocSIS8300/st.cmd
@@ -10,41 +10,34 @@ epicsEnvSet("PREFIX", "SIS8300:")
 epicsEnvSet("PORT",   "SIS8300")
 # The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
-# The maximim image width; used for row profiles in the NDPluginStats plugin
-epicsEnvSet("XSIZE",  "10")
-# The maximim image height; used for column profiles in the NDPluginStats plugin
-epicsEnvSet("YSIZE",  "2000")
 # The maximum number of time series points in the NDPluginStats plugin
-epicsEnvSet("NCHANS", "2048")
-# The maximum number of time series points in the NDPluginTimeSeries plugin
-epicsEnvSet("TSPOINTS", "2048")
+epicsEnvSet("NCHANS", "64")
 # The maximum number of frames buffered in the NDPluginCircularBuff plugin
 epicsEnvSet("CBUFFS", "500")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
-epicsEnvSet("AI1",  "AI1")
-epicsEnvSet("AI2",  "AI2")
-epicsEnvSet("AI3",  "AI3")
-epicsEnvSet("AI4",  "AI4")
-epicsEnvSet("AI5",  "AI5")
-epicsEnvSet("AI6",  "AI6")
-epicsEnvSet("AI7",  "AI7")
-epicsEnvSet("AI8",  "AI8")
-epicsEnvSet("AI9",  "AI9")
-epicsEnvSet("AI10", "AI10")
+epicsEnvSet("AICH0",      "AI0")
+epicsEnvSet("AICH1",      "AI1")
+epicsEnvSet("AICH2",      "AI2")
+epicsEnvSet("AICH3",      "AI3")
+epicsEnvSet("AICH4",      "AI4")
+epicsEnvSet("AICH5",      "AI5")
+epicsEnvSet("AICH6",      "AI6")
+epicsEnvSet("AICH7",      "AI7")
+epicsEnvSet("AICH8",      "AI8")
+epicsEnvSet("AICH9",      "AI9")
+
+# This is sum of AI and BPM asyn addresses
+# ADDR 0 .. 9 are for AI
+epicsEnvSet("NUM_CH",        "10")
+# Number of samples to acquire
+epicsEnvSet("NUM_SAMPLES",   "1024")
+# The maximum number of time series points in the NDPluginTimeSeries plugin
+epicsEnvSet("TSPOINTS",      "600000")
 
 asynSetMinTimerPeriod(0.001)
 
-# The EPICS environment variable EPICS_CA_MAX_ARRAY_BYTES needs to be set to a value at least as large
-# as the largest image that the standard arrays plugin will send.
-# That vlaue is $(XSIZE) * $(YSIZE) * sizeof(FTVL data type) for the FTVL used when loading the NDStdArrays.template file.
-# The variable can be set in the environment before running the IOC or it can be set here.
-# It is often convenient to set it in the environment outside the IOC to the largest array any client 
-# or server will need.  For example 10000000 (ten million) bytes may be enough.
-# If it is set here then remember to also set it outside the IOC for any CA clients that need to access the waveform record.  
-# Do not set EPICS_CA_MAX_ARRAY_BYTES to a value much larger than that required, because EPICS Channel Access actually
-# allocates arrays of this size every time it needs a buffer larger than 16K.
 # Uncomment the following line to set it in the IOC.
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")
 
@@ -52,62 +45,79 @@ epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")
 # SIS8300Config(const char *portName, const char *devicePath,
 #            int maxAddr, int numParams, int numTimePoints, NDDataType_t dataType,
 #            int maxBuffers, size_t maxMemory, int priority, int stackSize)
-SIS8300Config("$(PORT)", "/dev/sis8300-12", $(XSIZE), $(YSIZE), 7, 0, 0)
-dbLoadRecords("$(ADSIS8300)/db/SIS8300.template",  "P=$(PREFIX),R=,        PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI1):, PORT=$(PORT),ADDR=0,TIMEOUT=1,NAME=$(AI1)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI2):, PORT=$(PORT),ADDR=1,TIMEOUT=1,NAME=$(AI2)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI3):, PORT=$(PORT),ADDR=2,TIMEOUT=1,NAME=$(AI3)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI4):, PORT=$(PORT),ADDR=3,TIMEOUT=1,NAME=$(AI4)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI5):, PORT=$(PORT),ADDR=4,TIMEOUT=1,NAME=$(AI5)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI6):, PORT=$(PORT),ADDR=5,TIMEOUT=1,NAME=$(AI6)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI7):, PORT=$(PORT),ADDR=6,TIMEOUT=1,NAME=$(AI7)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI8):, PORT=$(PORT),ADDR=7,TIMEOUT=1,NAME=$(AI8)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI9):, PORT=$(PORT),ADDR=8,TIMEOUT=1,NAME=$(AI9)")
-dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AI10):,PORT=$(PORT),ADDR=9,TIMEOUT=1,NAME=$(AI10)")
+SIS8300Config("$(PORT)", "/dev/sis8300-12", $(NUM_CH), $(NUM_SAMPLES), 7, 0, 0)
+dbLoadRecords("$(ADSIS8300)/db/SIS8300.template",  "P=$(PREFIX),R=,          PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH0):, PORT=$(PORT),ADDR=0,TIMEOUT=1,NAME=$(AICH0)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH1):, PORT=$(PORT),ADDR=1,TIMEOUT=1,NAME=$(AICH1)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH2):, PORT=$(PORT),ADDR=2,TIMEOUT=1,NAME=$(AICH2)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH3):, PORT=$(PORT),ADDR=3,TIMEOUT=1,NAME=$(AICH3)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH4):, PORT=$(PORT),ADDR=4,TIMEOUT=1,NAME=$(AICH4)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH5):, PORT=$(PORT),ADDR=5,TIMEOUT=1,NAME=$(AICH5)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH6):, PORT=$(PORT),ADDR=6,TIMEOUT=1,NAME=$(AICH6)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH7):, PORT=$(PORT),ADDR=7,TIMEOUT=1,NAME=$(AICH7)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH8):, PORT=$(PORT),ADDR=8,TIMEOUT=1,NAME=$(AICH8)")
+dbLoadRecords("$(ADSIS8300)/db/SIS8300N.template", "P=$(PREFIX),R=$(AICH9):, PORT=$(PORT),ADDR=9,TIMEOUT=1,NAME=$(AICH9)")
 
-# Create a standard arrays plugin, set it to get data from SIS8300 driver.
+# Create a standard arrays plugin, set it to get data from ADSIS8300 driver.
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-# This creates a waveform large enough for 100000x10 arrays.
-dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Float64,FTVL=DOUBLE,NELEMENTS=1000000")
+# This creates a waveform large enough for 100x10 arrays.
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Float64,FTVL=DOUBLE,NELEMENTS=1000")
 
-# Time series plugin
-NDTimeSeriesConfigure("TS1", $(QSIZE), 0, "$(PORT)", 0, 10)
-dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template",  "P=$(PREFIX),R=TS:,   PORT=TS1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TimeStep CP MS,ENABLED=1")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:1:, PORT=TS1,ADDR=0,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI1)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:2:, PORT=TS1,ADDR=1,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI2)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:3:, PORT=TS1,ADDR=2,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI3)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:4:, PORT=TS1,ADDR=3,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI4)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:5:, PORT=TS1,ADDR=4,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI5)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:6:, PORT=TS1,ADDR=5,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI6)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:7:, PORT=TS1,ADDR=6,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI7)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:8:, PORT=TS1,ADDR=7,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI8)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:9:, PORT=TS1,ADDR=8,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI9)")
-dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS:10:,PORT=TS1,ADDR=9,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AI10)")
+# Time series plugin for converted AI data
+NDTimeSeriesConfigure("TS0", $(QSIZE), 0, "$(PORT)", 0, 10)
+dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template",  "P=$(PREFIX),R=TS0:,   PORT=TS0,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TimeStep CP MS,ENABLED=1")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:0:, PORT=TS0,ADDR=0,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH0)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:1:, PORT=TS0,ADDR=1,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH1)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:2:, PORT=TS0,ADDR=2,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH2)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:3:, PORT=TS0,ADDR=3,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH3)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:4:, PORT=TS0,ADDR=4,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH4)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:5:, PORT=TS0,ADDR=5,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH5)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:6:, PORT=TS0,ADDR=6,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH6)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:7:, PORT=TS0,ADDR=7,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH7)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:8:, PORT=TS0,ADDR=8,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH8)")
+dbLoadRecords("$(ADCORE)/db/NDTimeSeriesN.template", "P=$(PREFIX),R=TS0:9:, PORT=TS0,ADDR=9,TIMEOUT=1,NCHANS=$(TSPOINTS),NAME=$(AICH9)")
 
 # FFT plugins
-NDFFTConfigure("FFT1", $(QSIZE), 0, "TS1", 0)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT1:,PORT=FFT1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=0,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI1)")
-NDFFTConfigure("FFT2", $(QSIZE), 0, "TS1", 1)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT2:,PORT=FFT2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=1,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI2)")
-NDFFTConfigure("FFT3", $(QSIZE), 0, "TS1", 2
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT3:,PORT=FFT3,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=2,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI3)")
-NDFFTConfigure("FFT4", $(QSIZE), 0, "TS1", 3)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT4:,PORT=FFT4,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=3,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI4)")
-NDFFTConfigure("FFT5", $(QSIZE), 0, "TS1", 4)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT5:,PORT=FFT5,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=4,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI5)")
-NDFFTConfigure("FFT6", $(QSIZE), 0, "TS1", 5)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT6:,PORT=FFT6,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=5,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI6)")
-NDFFTConfigure("FFT7", $(QSIZE), 0, "TS1", 6)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT7:,PORT=FFT7,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=6,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI7)")
-NDFFTConfigure("FFT8", $(QSIZE), 0, "TS1", 7)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT8:,PORT=FFT8,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=7,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI8)")
-NDFFTConfigure("FFT9", $(QSIZE), 0, "TS1", 8)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT9:,PORT=FFT9,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=8,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI9)")
-NDFFTConfigure("FFT10", $(QSIZE), 0, "TS1", 9)
-dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT10:,PORT=FFT10,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS1,NDARRAY_ADDR=9,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AI10)")
+NDFFTConfigure("FFT0", $(QSIZE), 0, "TS0", 0)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT0:,PORT=FFT0,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=0,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH0)")
+NDFFTConfigure("FFT1", $(QSIZE), 0, "TS0", 1)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT1:,PORT=FFT1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=1,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH1)")
+NDFFTConfigure("FFT2", $(QSIZE), 0, "TS0", 2
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT2:,PORT=FFT2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=2,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH2)")
+NDFFTConfigure("FFT3", $(QSIZE), 0, "TS0", 3)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT3:,PORT=FFT3,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=3,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH3)")
+NDFFTConfigure("FFT4", $(QSIZE), 0, "TS0", 4)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT4:,PORT=FFT4,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=4,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH4)")
+NDFFTConfigure("FFT5", $(QSIZE), 0, "TS0", 5)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT5:,PORT=FFT5,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=5,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH5)")
+NDFFTConfigure("FFT6", $(QSIZE), 0, "TS0", 6)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT6:,PORT=FFT6,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=6,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH6)")
+NDFFTConfigure("FFT7", $(QSIZE), 0, "TS0", 7)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT7:,PORT=FFT7,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=7,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH7)")
+NDFFTConfigure("FFT8", $(QSIZE), 0, "TS0", 8)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT8:,PORT=FFT8,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=8,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH8)")
+NDFFTConfigure("FFT9", $(QSIZE), 0, "TS0", 9)
+dbLoadRecords("$(ADCORE)/db/NDFFT.template","P=$(PREFIX),R=FFT9:,PORT=FFT9,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TS0,NDARRAY_ADDR=9,NCHANS=$(TSPOINTS),TIME_LINK=$(PREFIX)TS:TSAveragingTime_RBV CP MS,ENABLED=1,NAME=$(AICH9)")
+
+
+# Timing MTCA EVR 300
+# As per EVR MTCA 300 engineering manual ch 5.3.5
+epicsEnvSet("SYS"               "EVR")
+epicsEnvSet("DEVICE"            "MTCA")
+epicsEnvSet("EVR_PCIDOMAIN"     "0x0")
+epicsEnvSet("EVR_PCIBUS"        "0x6")
+epicsEnvSet("EVR_PCIDEVICE"     "0x0")
+epicsEnvSet("EVR_PCIFUNCTION"   "0x0")
+
+mrmEvrSetupPCI($(DEVICE), $(EVR_PCIDOMAIN), $(EVR_PCIBUS), $(EVR_PCIDEVICE), $(EVR_PCIFUNCTION))
+dbLoadRecords("$(MRFIOC2)/db/evr-mtca-300.db", "DEVICE=$(DEVICE), SYS=$(SYS), Link-Clk-SP=88.0525")
+
+dbLoadRecords("$(MRFIOC2)/db/evr-softEvent.template", "DEVICE=$(DEVICE), SYS=$(SYS), EVT=14, CODE=14")
+dbLoadRecords("$(MRFIOC2)/db/evr-pulserMap.template", "DEVICE=$(DEVICE), SYS=$(SYS), PID=0, F=Trig, ID=0, EVT=14")
 
 ## Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
+
 set_requestfile_path("$(ADSIS8300)/SIS8300App/Db")
 
 #asynSetTraceIOMask("$(PORT)",0,2)
@@ -115,5 +125,16 @@ set_requestfile_path("$(ADSIS8300)/SIS8300App/Db")
 
 iocInit()
 
-# save things every thirty seconds
-create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+
+
+# Disable Rear Universal Output 33
+dbpf $(SYS)-$(DEVICE):RearUniv33-Ena-SP "Disabled"
+# Map Rear Universal Output 33 to pulser o
+dbpf $(SYS)-$(DEVICE):RearUniv33-Src-SP 0
+# Map pulser 0 to event 14
+dbpf $(SYS)-$(DEVICE):Pul0-Evt-Trig0-SP 14
+# Set pulser 0 width to 10 ms
+dbpf $(SYS)-$(DEVICE):Pul0-Width-SP 10000
+# Watch closely to the graph in the CSS OPI screen when enabling Rear Universal Output 33, since
+# in the next event 14 received the SIS8300 will start the data acquisition
+dbpf $(SYS)-$(DEVICE):RearUniv33-Ena-SP "Enabled"
